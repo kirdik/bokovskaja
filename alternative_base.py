e0b96c0efa05d5d6d6d70165a8b0404b924c3726
BuildTools <unconfigured@null.spigotmc.org>
2019-10-17 12:49:07 +0300

    test2

--- alternative_base.py
+++ alternative_base.py
@@ -4,11 +4,18 @@ class Person:
     do_death = '1941-01-01'
     mtr_unit = 'Неизвестно'
     ro_death = 'Неизвестно'
+    rip_loc = 'Неизвестно'
     village = 'Неизвестно'
     name = 'Неизвестно'
     surname = 'Неизвестно'
     patronymic = 'Неизвестно'
     address = 'Неизвестно'
+    godfather = 'Неизвестно'
+    godmother = 'Неизвестно'
+    father = 'Неизвестно'
+    mother = 'Неизвстно'
+    priest = 'Неизвестно'
+
 
     def fio(self, village, name, surname, patronymic, do_birth, address):
         self.village = village
@@ -24,9 +31,20 @@ class Person:
 
 
 class Soldier(Person):
-    def data_soldier(self,  mtr_unit, do_death, ro_death):
+    def data_soldier(self,  mtr_unit, do_death, ro_death, rip_loc):
         self.mtr_unit = mtr_unit
         self.do_death = do_death
         self.ro_death = ro_death
+        self.rio_loc = rip_loc
+
+class Church(Person):
+    def metrika(self, godfather, godmother, father, mother, priest):
+        self.godfather = godfather
+        self.godmother = godmother
+        self.father = father
+        self.mother = mother
+        self.priest = priest
+
+
 
 
