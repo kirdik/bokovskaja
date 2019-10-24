126c5e7a8daff50a528c377a3d7c0d10f434617c
BuildTools <unconfigured@null.spigotmc.org>
2019-10-24 15:03:30 +0300

    Class Queries

--- sql.py
+++ sql.py
@@ -1,11 +1,15 @@
 import sqlite3
-
 con = sqlite3.connect('base.sqlite')
 curs = con.cursor()
-sql_c_person = 'CREATE TABLE person (village, name, surname, patronymic, do_birth, address)'
-sql_c_soldier = ' CREATE TABLE soldier (mtr_unit, do_death, ro_death, rip_loc)'
-sql_c_church = ' CREATE TABLE churching ( goodfather, goodmother, father, mother, priest ) '
-sql_add_person = ' INSERT INTO person VALUES (?, ?, ?, ?, ?, ?), person '
+
+class Queries:
+
+    #sql_c_person = 'CREATE TABLE person (village, name, surname, patronymic, do_birth, address)'
+    #sql_c_soldier = ' CREATE TABLE soldier (mtr_unit, do_death, ro_death, rip_loc)'
+    #sql_c_church = ' CREATE TABLE churching ( goodfather, goodmother, father, mother, priest ) '
+    sql_add_p = ' INSERT INTO person VALUES (?, ?, ?, ?, ?, ?), person '
+    sql_add_s = 'INSERT INTO soldier VALUES (?, ?, ?, ?, ?), soldier'
+    sql_add_c = 'INSERT INTO churching VALUES (?, ?, ?, ?, ?, ? ), church'
 
 
 
